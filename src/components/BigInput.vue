961a5fdf203c4a6ec0927c4839ea4c606891aa9d
LoicMonard <monard@intechinfo.fr>
2022-12-02 05:37:52 +0100

    tech(input): refactor BigInput component to match with the API schema
    Signed-off-by: LoicMonard <monard@intechinfo.fr>

--- src/components/BigInput.vue
+++ src/components/BigInput.vue
@@ -1,36 +1,52 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import IconCommunity from './icons/IconCommunity.vue';
 
-const message = ref('');
-const pseudo = ref('');
-const showPseudo = ref(false);
-const pseudoInputDom = ref(null);
+const text = ref('');
+const author = ref('');
+const textInputDom = ref(null);
 
 const onKeyupEnter = () => {
-	showPseudo.value = true;
-	pseudoInputDom.value.focus();
+	textInputDom.value.focus();
 };
+
+const getAuthorFromLocalStorage = () => {
+	const authorFromLocalStorage = localStorage.getItem('author');
+	if (authorFromLocalStorage) {
+		author.value = authorFromLocalStorage;
+		textInputDom.value.focus();
+	}
+};
+
+const onAuthorChange = (author) => {
+	localStorage.setItem('author', author);
+};
+
+onMounted(() => {
+	getAuthorFromLocalStorage();
+});
 </script>
 
 <template>
-	<div class="flex flex-row gap-4 justify-center items-center px-8 py-6 shadow-xl rounded-lg bg-white overflow-hidden">
+	<div
+		class="flex flex-row min-h-[80px] gap-4 justify-center items-center px-8 py-6 shadow-xl rounded-2xl bg-white overflow-hidden">
 		<IconCommunity class="text-slate-400" />
-		<input
-			type="text"
-			v-model="message"
-			@keyup.enter="onKeyupEnter"
-			placeholder="Ask question..."
-			class="grow border-none outline-none" />
-		<div class="flex flex-row items-center gap-4 h-full transition-all duration-300" :class="{ '': showPseudo }">
-			<div class="border-r-2 border-slate-200 h-8"></div>
+		<div class="flex flex-row items-center gap-4 h-full transition-all duration-300">
 			<input
 				type="text"
-				v-model="pseudo"
+				v-model="author"
+				@keyup.enter="onKeyupEnter"
+				@change="onAuthorChange(author)"
 				placeholder="Pseudo"
-				@keyup.enter="$emit('messageSubmit', { message, pseudo })"
-				ref="pseudoInputDom"
-				class="grow border-none outline-none" />
+				class="grow border-none outline-none max-w-[100px]" />
 		</div>
+		<div class="border-r-2 border-slate-200 h-8"></div>
+		<input
+			type="text"
+			v-model="text"
+			@keyup.enter="$emit('messageSubmit', { text, author, messages: [] })"
+			ref="textInputDom"
+			placeholder="Ask question..."
+			class="grow border-none outline-none" />
 	</div>
 </template>
